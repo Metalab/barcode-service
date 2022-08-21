1dd10bad2e66186fd953062ad2b5c6228090a80d
Andreas Monitzer <a.monitzer@dungeonfog.com>
2022-08-21 22:49:08 +0200

    Enable pedantic clippy for the whole server.

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#[allow(clippy::pedantic)]
+#![allow(clippy::pedantic)]
 use std::path::PathBuf;
 
 use anyhow::Error;
